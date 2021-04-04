526db34d04b4a8d0939797efbbf5c26076862de5
Jocelyne Murphy <jomurphy@auvik.com>
2021-04-04 02:06:40 -0400

    image gallery

--- src/assets/photos.js
+++ src/assets/photos.js
@@ -1,47 +1,521 @@
+// an insane numbber of manual imports
+import lettering_be_authentic from '../assets/artphotos/authentic.jpg';
+import scene_landscape_sunset from '../assets/artphotos/Blue_Monaco.png';
+import technical_sectional_castle from '../assets/artphotos/a4-object-sectional.png';
+import abstract_line_face_sketch from '../assets/artphotos/IMG_0733.JPG';
+import abstract_line_animal_sketch from '../assets/artphotos/IMG_0734.JPG';
+import abstract_bodyparts_colourful_watercolour from '../assets/artphotos/IMG_0735.JPG';
+import face_highlighter from '../assets/artphotos/IMG_0736.JPG';
+import scene_rainbowglue from '../assets/artphotos/scan.jpg';
+import face_copic_autumn_blonde from '../assets/artphotos/scan0003.jpg';
+import treeperson_pencilcrayon from '../assets/artphotos/scan0004.jpg';
+import face_tropical from '../assets/artphotos/scan0005.jpg';
+import abstract_fluffy_fireworks from '../assets/artphotos/scan0006.jpg';
+import illustration_jordan_baby from '../assets/artphotos/scan0007.jpg';
+import acrylic_wolf_wood from '../assets/artphotos/scan0008.jpg';
+import copic_wine_orange_pants from '../assets/artphotos/scan0009.jpg';
+import watercolour_leaves_rainbow from '../assets/artphotos/scan0010.jpg';
+import face_copic_space_buns from '../assets/artphotos/scan0011.jpg';
+import face_sketch_man_brain_elf from '../assets/artphotos/scan0012.jpg';
+import acrylic_grey_tears from '../assets/artphotos/scan0013.jpg';
+import face_sketch_coolgirl_braids from '../assets/artphotos/scan0014.jpg';
+import abstract_line_rainbow_flowdance from '../assets/artphotos/scan0015.jpg';
+import character_quirky_gogglegirl from '../assets/artphotos/scan0017.jpg';
+import face_sketch_carefreeglasses from '../assets/artphotos/scan0018.jpg';
+import scene_lineart_universe_bow from '../assets/artphotos/scan0019.jpg';
+import face_3_greenorangepurple from '../assets/artphotos/scan0020.jpg';
+import lineart_polar_bear_ from '../assets/artphotos/scan0022.jpg';
+import sketch_violin from '../assets/artphotos/scan0023.jpg';
+import scene_cqe from '../assets/artphotos/scan0024.jpg';
+import copic_face_galaxy_freckles from '../assets/artphotos/scan0026.jpg';
+import phone_tropical_fruit from '../assets/artphotos/scan0027.jpg';
+import character_fruitsaladsmile from '../assets/artphotos/scan0028.jpg';
+import lettering_xmascard from '../assets/artphotos/scan0029.jpg';
+import fruitfly from '../assets/artphotos/scan0030.jpg';
+import lineart_tree_squiggleline from '../assets/artphotos/scan0031.jpg';
+import character_quirky_bodyparts3 from '../assets/artphotos/scan0032.jpg';
+import face_brownorange from '../assets/artphotos/scan0033.jpg';
+import lineart_pen_tree from '../assets/artphotos/scan0034.jpg';
+import sketch_fruit_negativeeraser_lighting from '../assets/artphotos/scan0035.jpg';
+import technical_elementsofdesign from '../assets/artphotos/scan0036.jpg';
+import technical_principlesofdesign from '../assets/artphotos/scan0037.jpg';
+import lineart_sketch_scene from '../assets/artphotos/scan0038.jpg';
+import lineart_chameleon from '../assets/artphotos/scan0039.jpg';
+import abstract_marker_makeuptriangles from '../assets/artphotos/scan0040.jpg';
+import abstract_mushynaturefaces from '../assets/artphotos/scan0041.jpg';
+import lettering_cactus1 from '../assets/artphotos/scan0042.jpg';
+import lettering_cactus1_background from '../assets/artphotos/scan0043.jpg';
+import lettering_cactus2 from '../assets/artphotos/scan0044.jpg';
+import face_watercolour_50s_rainbowgirl from '../assets/artphotos/scan0047.jpg';
+import face_watercolour_cheetah from '../assets/artphotos/scan0048.jpg';
+import face_watercolour_plants from '../assets/artphotos/scan0049.jpg';
+import abstract_closeup_lighting_orangeteal from '../assets/artphotos/scan0052.jpg';
+import scene_campfire from '../assets/artphotos/scan0055.jpg';
+import scene_sunsetcanoe from '../assets/artphotos/scan0056.jpg';
+import scene_plane from '../assets/artphotos/scan0058.jpg';
+import scene_palmtrees from '../assets/artphotos/scan0059.jpg';
+import scene_helicopter_rescue from '../assets/artphotos/scan0060.jpg';
+import scene_bownarrow_construction from '../assets/artphotos/scan0061.jpg';
+import scene_sinking_plane from '../assets/artphotos/scan0062.jpg';
+import character_lalala from '../assets/artphotos/scan0065.jpg';
+import lineart_elephant from '../assets/artphotos/scan0066.jpg';
+import lineart_character_buns_raincoat from '../assets/artphotos/scan0067.jpg';
+import lineart_emmabraids from '../assets/artphotos/scan0068.jpg';
+import character_sassy_curvy from '../assets/artphotos/Screenshot (130).png';
+import copic_face_galaxyfreckles_progression from '../assets/artphotos/Screenshot (131).png';
+import abstract_face_highlighter_vividsharp from '../assets/artphotos/Screenshot (132).png';
+import abstract_face_bodyparts from '../assets/artphotos/Screenshot (133).png';
+import characters_WOO_grassskirt from '../assets/artphotos/Screenshot (134).png';
+import watercolour_juicebox from '../assets/artphotos/Screenshot (135).png';
+import lettering_seizetheday from '../assets/artphotos/seize.jpg';
+import lettering_timeislimited from '../assets/artphotos/time.jpg';
+import scene_space from '../assets/artphotos/space.png';
+import character_kooky_backpack from '../assets/artphotos/kooky1.png';
+import character_kooky_flipflop from '../assets/artphotos/kooky2.png';
+import character_kooky_chef from '../assets/artphotos/kooky3.png';
+import character_kooky_scream from '../assets/artphotos/kooky4.png';
+import character_kooky_worried from '../assets/artphotos/kooky5.png';
+import character_kooky_braids from '../assets/artphotos/kooky6.png';
+import character_kooky_drippy from '../assets/artphotos/kooky7.png';
+import lineart_abstract_oneline2 from '../assets/artphotos/oneline2.png';
+import lineart_abstract_oneline3 from '../assets/artphotos/oneline3.png';
+import lineart_onelinerobo from '../assets/artphotos/onelinerobo.png';
+import character_explotoilet from '../assets/artphotos/exploilet.png';
+import wonk_lighting from '../assets/artphotos/wonk.jpg';
+import abstract_geowatercolor_face from '../assets/artphotos/geowatercolour.png';
+import lettering_peaches from '../assets/artphotos/peaches.png';
+import face_misty from '../assets/artphotos/misty.png';
+
 export const photos = [
   {
-    src: 'artphotos/authentic.jpg',
+    src: abstract_closeup_lighting_orangeteal,
+    width: 3,
+    height: 3,
+  },
+  {
+    src: copic_face_galaxy_freckles,
+    width: 7,
+    height: 10,
+  },
+  {
+    src: scene_space,
+    width: 3,
+    height: 4,
+  },
+  {
+    src: sketch_violin,
     width: 4,
     height: 3,
   },
   {
-    src: 'https://source.unsplash.com/Dm-qxdynoEc/800x799',
+    src: fruitfly,
+    width: 5,
+    height: 3,
+  },
+  {
+    src: character_quirky_bodyparts3,
+    width: 3,
+    height: 3,
+  },
+  {
+    src: character_explotoilet,
+    width: 3,
+    height: 4,
+  },
+  {
+    src: scene_sunsetcanoe,
+    width: 3,
+    height: 5,
+  },
+  {
+    src: scene_landscape_sunset,
+    width: 4,
+    height: 3,
+  },
+  {
+    src: scene_palmtrees,
+    width: 3,
+    height: 5,
+  },
+  {
+    src: lineart_chameleon,
+    width: 6,
+    height: 3,
+  },
+  {
+    src: face_tropical,
+    width: 3,
+    height: 3,
+  },
+  {
+    src: character_kooky_drippy,
+    width: 2,
+    height: 4,
+  },
+  {
+    src: character_quirky_gogglegirl,
+    width: 3,
+    height: 4,
+  },
+  // {
+  //   src: lineart_sketch_scene,
+  //   width: 4,
+  //   height: 3,
+  // },
+  {
+    src: wonk_lighting,
+    width: 5,
+    height: 6,
+  },
+  // {
+  //   src: abstract_marker_makeuptriangles,
+  //   width: 4,
+  //   height: 3,
+  // },
+  {
+    src: face_watercolour_50s_rainbowgirl,
+    width: 3,
+    height: 4,
+  },
+  {
+    src: abstract_line_animal_sketch,
+    width: 3,
+    height: 4,
+  },
+  {
+    src: scene_cqe,
+    width: 4,
+    height: 3,
+  },
+  // {
+  //   src: face_sketch_coolgirl_braids,
+  //   width: 14,
+  //   height: 15,
+  // },
+  {
+    src: abstract_geowatercolor_face,
+    width: 3,
+    height: 4,
+  },
+  {
+    src: watercolour_juicebox,
+    width: 3,
+    height: 3,
+  },
+  {
+    src: lineart_character_buns_raincoat,
+    width: 3,
+    height: 6,
+  },
+  {
+    src: character_kooky_chef,
+    width: 3,
+    height: 5,
+  },
+
+  {
+    src: watercolour_leaves_rainbow,
+    width: 4,
+    height: 3,
+  },
+  // {
+  //   src: lettering_be_authentic,
+  //   width: 5,
+  //   height: 3,
+  // },
+
+  {
+    src: scene_lineart_universe_bow,
+    width: 4,
+    height: 3,
+  },
+  {
+    src: character_fruitsaladsmile,
+    width: 8,
+    height: 3,
+  },
+
+  // {
+  //   src: abstract_mushynaturefaces,
+  //   width: 4,
+  //   height: 3,
+  // },
+  {
+    src: scene_helicopter_rescue,
+    width: 3,
+    height: 5,
+  },
+  {
+    src: abstract_bodyparts_colourful_watercolour,
+    width: 3,
+    height: 4,
+  },
+
+  {
+    src: scene_campfire,
+    width: 3,
+    height: 5,
+  },
+  {
+    src: character_sassy_curvy,
+    width: 3,
+    height: 6,
+  },
+  {
+    src: face_sketch_man_brain_elf,
+    width: 3,
+    height: 4,
+  },
+  {
+    src: character_kooky_flipflop,
+    width: 2,
+    height: 4,
+  },
+  {
+    src: face_highlighter,
+    width: 3,
+    height: 4,
+  },
+  {
+    src: sketch_fruit_negativeeraser_lighting,
+    width: 4,
+    height: 3,
+  },
+  {
+    src: lineart_elephant,
+    width: 3,
+    height: 3,
+  },
+  {
+    src: lineart_tree_squiggleline,
+    width: 4,
+    height: 3,
+  },
+  {
+    src: abstract_line_face_sketch,
+    width: 3,
+    height: 4,
+  },
+  {
+    src: phone_tropical_fruit,
+    width: 3,
+    height: 4,
+  },
+  {
+    src: character_lalala,
+    width: 3,
+    height: 4,
+  },
+  {
+    src: lineart_emmabraids,
+    width: 3,
+    height: 5,
+  },
+  {
+    src: face_copic_autumn_blonde,
+    width: 4,
+    height: 3,
+  },
+  {
+    src: abstract_line_rainbow_flowdance,
+    width: 3,
+    height: 5,
+  },
+  {
+    src: scene_rainbowglue,
+    width: 4,
+    height: 3,
+  },
+  {
+    src: illustration_jordan_baby,
+    width: 3,
+    height: 4,
+  },
+  {
+    src: treeperson_pencilcrayon,
+    width: 3,
+    height: 4,
+  },
+  {
+    src: acrylic_wolf_wood,
+    width: 4,
+    height: 3,
+  },
+  {
+    src: acrylic_grey_tears,
+    width: 4,
+    height: 3,
+  },
+  {
+    src: lineart_abstract_oneline2,
+    width: 3,
+    height: 4,
+  },
+  {
+    src: lineart_polar_bear_,
+    width: 4,
+    height: 3,
+  },
+  // {
+  //   src: copic_wine_orange_pants,
+  //   width: 3,
+  //   height: 5,
+  // },
+  {
+    src: technical_elementsofdesign,
     width: 1,
-    height: 1,
+    height: 3,
   },
+  // {
+  //   src: abstract_fluffy_fireworks,
+  //   width: 5,
+  //   height: 1,
+  // },
+  // {
+  //   src: face_brownorange,
+  //   width: 3,
+  //   height: 4,
+  // },
   {
-    src: 'https://source.unsplash.com/qDkso9nvCg0/600x799',
+    src: face_watercolour_cheetah,
     width: 3,
     height: 4,
   },
   {
-    src: 'https://source.unsplash.com/iecJiKe_RNg/600x799',
+    src: face_watercolour_plants,
+    width: 6,
+    height: 7,
+  },
+  // {
+  //   src: lettering_seizetheday,
+  //   width: 4,
+  //   height: 3,
+  // },
+  {
+    src: face_copic_space_buns,
+    width: 6,
+    height: 9,
+  },
+  {
+    src: lettering_cactus1_background,
+    width: 3,
+    height: 3,
+  },
+  {
+    src: face_3_greenorangepurple,
     width: 3,
     height: 4,
   },
   {
-    src: 'https://source.unsplash.com/epcsn8Ed8kY/600x799',
+    src: scene_plane,
+    width: 3,
+    height: 5,
+  },
+  {
+    src: character_kooky_backpack,
+    width: 3,
+    height: 5,
+  },
+  {
+    src: characters_WOO_grassskirt,
+    width: 3,
+    height: 3,
+  },
+  // {
+  //   src: face_sketch_carefreeglasses,
+  //   width: 3,
+  //   height: 4,
+  // },
+  // {
+  //   src: scene_sinking_plane,
+  //   width: 3,
+  //   height: 5,
+  // },
+  // {
+  //   src: scene_bownarrow_construction,
+  //   width: 3,
+  //   height: 5,
+  // },
+  // {
+  //   src: lettering_xmascard,
+  //   width: 4,
+  //   height: 3,
+  // },
+  {
+    src: abstract_face_highlighter_vividsharp,
     width: 3,
     height: 4,
   },
   {
-    src: 'https://source.unsplash.com/NQSWvyVRIJk/800x599',
+    src: lineart_pen_tree,
+    width: 3,
+    height: 4,
+  },
+  {
+    src: abstract_face_bodyparts,
+    width: 3,
+    height: 4,
+  },
+  {
+    src: lineart_abstract_oneline3,
+    width: 3,
+    height: 4,
+  },
+  {
+    src: lettering_peaches,
+    width: 4,
+    height: 3,
+  },
+  // {
+  //   src: lettering_cactus2,
+  //   width: 4,
+  //   height: 3,
+  // },
+  {
+    src: lettering_cactus1,
+    width: 7,
+    height: 4,
+  },
+  // {
+  //   src: lettering_timeislimited,
+  //   width: 3,
+  //   height: 3,
+  // },
+  {
+    src: technical_sectional_castle,
+    width: 3,
+    height: 5,
+  },
+
+  // {
+  //   src: character_kooky_braids,
+  //   width: 3,
+  //   height: 3,
+  // },
+  {
+    src: character_kooky_scream,
+    width: 3,
+    height: 3,
+  },
+  // {
+  //   src: character_kooky_worried,
+  //   width: 1,
+  //   height: 1,
+  // },
+  {
+    src: copic_face_galaxyfreckles_progression,
+    width: 3,
+    height: 3,
+  },
+
+  {
+    src: lineart_onelinerobo,
     width: 4,
     height: 3,
   },
   {
-    src: 'https://source.unsplash.com/zh7GEuORbUw/600x799',
+    src: face_misty,
     width: 3,
-    height: 4,
-  },
-  {
-    src: 'https://source.unsplash.com/PpOHJezOalU/800x599',
-    width: 4,
-    height: 3,
-  },
-  {
-    src: 'https://source.unsplash.com/I1ASdgphUH4/800x599',
-    width: 4,
     height: 3,
   },
 ];
--- src/pages/art.js
+++ src/pages/art.js
@@ -1,9 +1,9 @@
+import { photos } from '../assets/photos';
 import React, { useState } from 'react';
 import Gallery from 'react-photo-gallery';
 import Photo from '../components/Photo';
 import arrayMove from 'array-move';
 import { SortableContainer, SortableElement } from 'react-sortable-hoc';
-import { photos } from '../assets/photos';
 import Header from '../components/Header';
 
 /* popout the browser and maximize to see more rows! -> */
@@ -12,17 +12,37 @@ const SortableGallery = SortableContainer(({ items }) => (
   <Gallery photos={items} renderImage={(props) => <SortablePhoto {...props} />} />
 ));
 
+// const shuffleGallery = (photos) => {
+//   shuffle(photos);
+// };
+
 export default function Art() {
-  const [items, setItems] = useState(photos);
-  const onSortEnd = ({ oldIndex, newIndex }) => {
-    setItems(arrayMove(items, oldIndex, newIndex));
-  };
+  // const [items, setItems] = useState(photos);
+  // const onSortEnd = ({ oldIndex, newIndex }) => {
+  //   setItems(arrayMove(items, oldIndex, newIndex));
+  // };
+  // console.log(photos);
+
+  // const [photos, setPhotos] = React.useState(shuffleGallery(photos));
 
   return (
     <div>
-      <img src="../assets/artphotos/authentic"></img>
       <Header></Header>
-      <Gallery photos={photos} direction={'column'} />;
+      <Gallery
+        photos={photos}
+        direction={'column'}
+        // onClick={() => {
+        //   for (let i = photos.length - 1; i > 0; i--) {
+        //     const j = Math.floor(Math.random() * i);
+        //     const temp = photos[i];
+        //     photos[i] = photos[j];
+        //     photos[j] = temp;
+        //   }
+
+        //   setPhotos(photos);
+        // }}
+      />
+      ;
       {/* <h2>Sortable Gallery</h2>
       <h3>Drag photo to rearrange</h3>
       <SortableGallery items={items} onSortEnd={onSortEnd} axis={'xy'} /> */}
