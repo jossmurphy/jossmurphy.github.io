5af8627be77a5b4bd38131d80098dc39d4ed9dfb
Jocelyne Murphy <jomurphy@auvik.com>
2021-04-03 21:12:49 -0400

    photos

--- src/pages/art.js
+++ src/pages/art.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Gallery from 'react-photo-gallery';
+import Photo from '../components/Photo';
+import arrayMove from 'array-move';
+import { SortableContainer, SortableElement } from 'react-sortable-hoc';
+import { photos } from '../assets/photos';
 import Header from '../components/Header';
 
+/* popout the browser and maximize to see more rows! -> */
+const SortablePhoto = SortableElement((item) => <Photo {...item} />);
+const SortableGallery = SortableContainer(({ items }) => (
+  <Gallery photos={items} renderImage={(props) => <SortablePhoto {...props} />} />
+));
+
 export default function Art() {
+  const [items, setItems] = useState(photos);
+  const onSortEnd = ({ oldIndex, newIndex }) => {
+    setItems(arrayMove(items, oldIndex, newIndex));
+  };
+
   return (
     <div>
+      <img src="../assets/artphotos/authentic"></img>
       <Header></Header>
+      <Gallery photos={photos} direction={'column'} />;
+      {/* <h2>Sortable Gallery</h2>
+      <h3>Drag photo to rearrange</h3>
+      <SortableGallery items={items} onSortEnd={onSortEnd} axis={'xy'} /> */}
     </div>
   );
 }
