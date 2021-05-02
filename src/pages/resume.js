780397498a2cfe016635d8b7c58a456feeba60d6
jossmurphy <43387820+jossmurphy@users.noreply.github.com>
2021-05-01 23:07:54 -0400

    fix errors

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,6 +1,5 @@
 import Header from '../components/Header';
 import React, { useState } from 'react';
-import { Document, Page, pdfjs } from 'react-pdf';
 import {
   PageTitle,
   Subtitle1,
@@ -15,31 +14,31 @@ import {
   CaseStudy,
   Hyperlink,
 } from '../styles/global.js';
-import Resume_pdf from '../assets/JocelyneMurphy_Resume.pdf';
-pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-
-export default function Resume() {
-  return (
-    <div>
-      <Header></Header>
-      <CaseStudy>
-        <Subtitle1>Welcome!</Subtitle1>
-        <Button primary>
-          <A href={Resume_pdf} target="blank">
-            Projects
-          </A>
-        </Button>
-        <Document file={Resume_pdf}></Document>
-      </CaseStudy>
-    </div>
-  );
-}
+// import Resume_pdf from '../assets/JocelyneMurphy_Resume.pdf';
+// pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 // export default function Resume() {
 //   return (
 //     <div>
 //       <Header></Header>
-//       <Document file="JocelyneMurphy_Resume.pdf" />
+//       <CaseStudy>
+//         <Subtitle1>Welcome!</Subtitle1>
+//         <Button primary>
+//           <A href={Resume_pdf} target="blank">
+//             Projects
+//           </A>
+//         </Button>
+//         <Document file={Resume_pdf}></Document>
+//       </CaseStudy>
 //     </div>
 //   );
 // }
+
+export default function Resume() {
+  return (
+    <div>
+      <Header></Header>
+      {/* <Document file="JocelyneMurphy_Resume.pdf" /> */}
+    </div>
+  );
+}
