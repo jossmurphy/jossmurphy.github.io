fe76c6c1c9afb17191d60c90ef5a34758cbd3912
jossmurphy <43387820+jossmurphy@users.noreply.github.com>
2021-05-03 12:47:46 -0400

    bug fixes

--- src/components/YoutubeEmbed.tsx
+++ src/components/YoutubeEmbed.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const YoutubeEmbed = ({ embedId }) => (
+const YoutubeEmbed = () => (
   <div className="video-responsive">
     <iframe
       width="853"
       height="480"
-      src={`https://www.youtube.com/embed/${embedId}`}
+      // src={`https://www.youtube.com/embed/${embedId}`}
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
